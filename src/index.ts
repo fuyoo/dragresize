c0ad96f45358ebabc274cb3b407d2c6c0792039c
fuyoo <fuyoo0928@gmail.com>
2023-03-10 11:50:46 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import VueDraggableResizable from './components/DragResize'
-import DraggableContainer from './components/DragResizeContainer'
+import DragResize from './components/DragResize'
+import DragResizeContainer from './components/DragResizeContainer'
 import { App, Plugin } from 'vue'
 
-VueDraggableResizable.install = (app: App) => {
-  app.component(VueDraggableResizable.name, VueDraggableResizable)
-  app.component(DraggableContainer.name, DraggableContainer)
+DragResize.install = (app: App) => {
+  app.component(DragResize.name, DragResize)
+  app.component(DragResizeContainer.name, DragResizeContainer)
   return app
 }
 
-export { default as DraggableContainer } from './components/DragResizeContainer'
-export default VueDraggableResizable as typeof VueDraggableResizable & Plugin
+export { default as DragResizeContainer } from './components/DragResizeContainer'
+export default DragResize as typeof DragResize & Plugin
